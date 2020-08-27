4c65808a11478b2d81d7b9e45038a4d3a20683a4
Juliette Maës <maesneako@gmail.com>
2020-08-27 16:37:21 +0200

    Add: logger for db creation + relabeling depending on frequency - TODO: use functions to shuffle / split data

--- extract_data.py
+++ extract_data.py
@@ -2,9 +2,9 @@
 
 Examples:
     With training data (NewEngland corpus):
-    >>> python extract_data.py formatted/NewEngland/data.csv --output_format one_tsv --ttv_filepattern childes_{}.tsv --select_data file_id spa_1 spa_2 spa_2a speaker sentence
+    >>> python extract_data.py --csv_loc formatted/NewEngland/data.csv --output_format one_tsv --ttv_filepattern childes_{}.tsv --select_data file_id spa_1 spa_2 spa_2a speaker sentence
     With test data (Bates corpus):
-    >>> python extract_data.py formatted/Bates/data.csv --keep_empty_spa --output_format one_tsv --ttv_filepattern 'bates_{}.tsv'
+    >>> python extract_data.py --csv_loc formatted/Bates/data.csv --keep_empty_spa --output_format one_tsv --ttv_filepattern 'bates_{}.tsv'
 """
 import json
 import os, sys
@@ -14,34 +14,138 @@ import pandas as pd
 import time, datetime
 import argparse
 import re
+from sklearn.utils import shuffle
 
-from utils import *
+from utils import dataset_labels
+
+### Arguments
+def argparser():
+    """Create argparser, check arguments and return.
+    """
+    argparser = argparse.ArgumentParser(description='Extract and transform data.', formatter_class=argparse.RawTextHelpFormatter)
+    # Data files
+    argparser.add_argument('--csv_loc', type=str, default=None, help='location of the previously extracted data - csv file')
+    argparser.add_argument('--json_loc', type=str, default=None, help='location of json files with extracted data - MACANNOT files')
+    argparser.add_argument('--output_format', choices=['one_tsv', 'sep_txt'], help="""how to output the data based on the algorithm that will be applied next: 
+        * 'one_tsv': HDF5 preparation for RNN
+        * 'sep_txt': TXT preparation for CRF
+    """)
+    # Extraction parameters
+    argparser.add_argument('--thres_replace', type=int, default=0, help="tags with a number of occurrences under this threshold will be replaced by the 'NEE' tag (Not Enough Examples")
+    argparser.add_argument('--keep_untagged', action='store_true', help="whether to keep segments without spa tags (NOL) / with labels that don't meet the threshold (NEE) / with incorrect labels (NAT)")
+    argparser.add_argument('--select_data', choices=["utterance", "spa_all", "spa_1", "spa_2", "spa_2a", "time_stamp", "speaker", "sentence", "lemmas", "pos", "action", "file_id", "age_months", "translation", "child"], nargs='+', help="ordered features for output - will be adapted to generate 3 files for ")
+    # Actions on data
+    argparser.add_argument('--replace_names', action='store_true', help="whether to replace locutors callings by tag; applicable on columns 'sentence' and 'lemmas'")
+    argparser.add_argument('--match_age', type=int, nargs='+', default=None, help="ages to match data to - for split analysis")
+    # Amount of data
+    argparser.add_argument('--split_documents', choices=['no_split', 'on_blank', 'gaussian', 'exact'], default='no_split', help="whether to split the data into smaller documents; if any which splitting method to use")
+    argparser.add_argument('--duplicate_documents', action="store_true", help="whether to split the data into smaller documents using a rolling window (increasing training data)")
+    argparser.add_argument('--split_length', type=int, default=None, help="amount of lines (avg for gaussian) to put in split.")
+    # Train/test/validation
+    argparser.add_argument('--ttv_split', nargs=3, type=float, default=[0.7, 0.25, 0.05], help="percentage of files going to train/test/validation sets; if --keep_empty_spa is False, test is all of the data")
+    argparser.add_argument('--ttv_filepattern', type=str, default="list_{}_conv", help="file pattern (filled with train/test/valid + other patterns if need be) for ttv files - default 'list_{}_conv'.format(XXX)")
+    argparser.add_argument('--unlabeled_data', action='store_true', help='for unlabeled data, no speech act is associated to sentences; cannot filter on empty tags (takes precedence over --ttv_split)')
+
+    args = argparser.parse_args()
+    if args.csv_loc is None and args.json_loc is None:
+        raise ValueError("Data location must be given either through the --csv_loc or the --json_loc argument.")
+    elif args.json_loc is not None and not os.path.isdir(args.json_loc):
+        raise ValueError(f"{args.json_loc} is not a folder.")
+    # not checking csv file, could have other extension but correct format
+
+    if args.split_documents in ['gaussian', 'exact'] and args.split_length is None:
+        raise ValueError("--split_length must be set")
+    elif args.duplicate_documents and args.split_length is None:
+        raise ValueError("--split_length must be set")
+
+    if args.select_data is None:
+        if args.output_format == 'sep_txt':
+            args.select_data = ["spa_all", "utterance", "time_stamp", "speaker", "sentence"]
+        if args.output_format == 'one_tsv':
+            args.select_data = ['file_id', 'spa_1', 'spa_2', 'spa_2a', 'speaker', 'sentence']
+    
+    if args.unlabeled_data:
+        args.ttv_split = [0., 1., 0.]
+
+    if args.match_age is not None and "age_months" not in args.select_data:
+        print("Adding 'age_months' column to output for consistency checks")
+        args.select_data.append("age_months")
+
+    return args
 
 
 ### WRITE DATA
-def sep_loop(df, line_function, ttv_writer, remove_empty_tags=True):
+def sep_loop(df:pd.DataFrame, line_function:list, ttv_writer:dict, remove_empty_tags:bool=True):
     # extract file <-> ttv columns, set as index, series to dict
     sets = df[['file_id', 'ttv']].drop_duplicates().set_index('file_id').to_dict()['ttv']
     # either remove empty tags or replace them with placeholder
     ss = [col for col in line_function if 'spa_' in col]
     if remove_empty_tags:
-        data.dropna(subset = ss, inplace=True)
-    else:
-        data[ss] = data[ss].fillna(value='None')
+        for col in ss:
+            # Note: hierarchical: all > 1 > 2 = 2a ; if one is empty the next are empty
+            df.drop(df[df[col].isin(['NOL', 'NAT', 'NEE'])].index, inplace=True) 
+    # No else, values are already replaced
     for k,v in sets.items():
         ttv_writer[v].write(k.replace('.json', '.txt')+'\n')
         df[df['file_id'] == k][line_function].to_csv(k.replace('.json', '.txt'), sep='\t', header=False, index=False)
 
-def one_loop(df, line_function, ttv_writer, remove_empty_tags=True):
+def one_loop(df:pd.DataFrame, line_function:list, ttv_writer:dict, remove_empty_tags:bool=True):
     sub_data = {k: df[df['ttv'] == k] for k in range(0,3)}
     for k,v in ttv_writer.items():
         for columns, filepath in zip(line_function, v):
-            tag = [col for col in columns if 'spa_' in col]
+            tag = [col for col in columns if 'spa_' in col][0] # only supposed to be one tag by list of columns
             if remove_empty_tags:
-                sub_data[k].dropna(subset = tag, inplace=True)
-            else:
-                sub_data[k][tag] = sub_data[k][tag].fillna(value='None')
+                # Note: hierarchical: 1 > 2 = 2a ; if one is empty the next are empty
+                sub_data[k].drop(sub_data[k][sub_data[k][tag].isin(['NOL', 'NAT', 'NEE'])].index, inplace=True) 
             sub_data[k][columns].rename(columns={col:col.upper() for col in columns}).to_csv(filepath, sep='\t', index=False)
+
+# Create more data
+def take_percent(data:pd.DataFrame, fraction:float, conv_column:str, 
+					split_avg_lgth:int = 50, split_var_lgth:int=10) -> pd.DataFrame:
+	"""Split the data into segments of average length split_avg_lgth, and randomize the extraction of data from the dataset only to keep a given fraction of data. 
+	column containing file references is updated to include splits, so that the algorithm doesn't later group data from one file all together despite splits.
+	"""
+	if (fraction > 1 or fraction < 0):
+		raise ValueError("Fraction {fraction} must be between 0. and 1..")
+	
+	n = data.shape[0]
+	split_index = np.cumsum(np.random.normal(split_avg_lgth, split_var_lgth, int(n/split_avg_lgth)).astype(int))
+	# update split index with data file changes index
+	file_idx = data[data[conv_column] != data[conv_column].shift(1)].index.tolist()
+	for f_idx in file_idx:
+		split_index[min(range(len(split_index)), key = lambda i: abs(split_index[i]-f_idx))] = f_idx
+	
+	# split and shuffle
+	tmp = []
+	for i, idx in enumerate(split_index[:-1]):
+		subset = data.iloc[idx:split_index[i+1], :]
+		subset[conv_column] = subset[conv_column].apply(lambda x: x+'_'+str(i))
+		tmp.append(subset)
+	tmp = shuffle(tmp)
+	tmp = pd.concat(tmp, axis=0)
+
+	# compute fraction and return
+	return tmp.iloc[:int(n*fraction), :]
+
+def create_rw_data(data:pd.DataFrame, conv_column:str, split_lgth:int = 50) -> pd.DataFrame:
+	"""Split the data into segments of length split_avg_lgth, and use a rolling window to create more training data. 
+	column containing file references is updated to include splits, so that the algorithm doesn't later group data from one file all together despite splits.
+	"""
+	# index of file change in data
+	file_idx = data[data[conv_column] != data[conv_column].shift(1)].index.tolist()
+	# create rolling windows
+	rw_idx = [[(a, min(a+split_lgth, file_idx[i+1])) for a in range(idx, file_idx[i+1], split_lgth)] for i, idx in enumerate(file_idx[:-1])]
+	rw_idx = [y for x in rw_idx for y in x] # flatten
+	tmp = []
+	for i, (idx_start, idx_end) in enumerate(rw_idx):
+		subset = data.iloc[idx_start:idx_end, :]
+		subset[conv_column] = subset[conv_column].apply(lambda x: x+'_'+str(i))
+		tmp.append(subset)
+	tmp = shuffle(tmp)
+	tmp = pd.concat(tmp, axis=0)
+
+	# return data
+	return tmp
 
 ### READ DATA
 def read_lines_from_folder(folder:str) -> pd.DataFrame:
@@ -65,73 +169,96 @@ def read_lines_from_folder(folder:str) -> pd.DataFrame:
                         # add spa_1, spa_2, spa_2a
                         # pos = '' if "pos" not in doc["tokens"][0].keys() else " ".join([ x["pos"] for x in doc["tokens"]])
                         # lemmas = '' if "lemmas" not in doc["tokens"][0].keys() else " ".join([ x["lemmas"] for x in doc["tokens"]])
-                        # action = 
+                        # action = ''
                     }
                 p.append(d)
     return pd.DataFrame(p)
 
+### UPDATE DATA
+def replace_tag(x:str, tag_incorr:list, tag_toofew:list):
+    if x == '' or x == 'NOL':
+        return 'NOL'
+    elif x in tag_incorr:
+        return 'NAT'
+    elif x in tag_toofew:
+        return 'NEE'
+    return x
+
+# Update file containing metadata
+def log_dataset(args):
+    fname = 'ttv/db_metadata.json'
+    entry = {}
+    for arg in vars(args):
+        entry[arg] = getattr(args, arg)
+    # Dumping data
+    a = []
+    if not os.path.isfile(fname):
+        a.append(entry)
+        with open(fname, mode='w') as f:
+            f.write(json.dumps(a, indent=2))
+    else:
+        with open(fname) as feedsjson:
+            feeds = json.load(feedsjson)
+
+        feeds.append(entry)
+        with open(fname, mode='w') as f:
+            f.write(json.dumps(feeds, indent=2))
+
+
 ### MAIN
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(description='Extract and transform data.', formatter_class=argparse.RawTextHelpFormatter)
-    # Data files
-    argparser.add_argument('--csv_loc', type=str, default=None, help='location of the previously extracted data - csv file')
-    argparser.add_argument('--json_loc', type=str, default=None, help='location of json files with extracted data - MACANNOT files')
-    argparser.add_argument('--output_format', choices=['one_tsv', 'sep_txt'], help="""how to output the data based on the algorithm that will be applied next: 
-        * 'one_tsv': HDF5 preparation for RNN
-        * 'sep_txt': TXT preparation for CRF
-    """)
-    # Extraction parameters
-    argparser.add_argument('--keep_empty_text', action='store_true', help='whether to keep segments without any speech (might have acts) in final data')
-    argparser.add_argument('--keep_empty_spa', action='store_true', help="whether to keep segments without spa tags")
-    argparser.add_argument('--select_data', choices=["utterance", "spa_all", "spa_1", "spa_2", "spa_2a", "time_stamp", "speaker", "sentence", "lemmas", "pos", "action", "file_id", "age_months", "translation"], nargs='+', help="ordered features for output - will be adapted to generate 3 files for ")
-    argparser.add_argument('--replace_names', action='store_true', help="whether to replace locutors callings by tag; applicable on columns 'sentence' and 'lemmas'")
-    # Train/test/validation
-    argparser.add_argument('--ttv_split', nargs=3, type=float, default=[0.7, 0.25, 0.05], help="percentage of files going to train/test/validation sets; if --keep_empty_spa is False, test is all of the data")
-    argparser.add_argument('--ttv_filepattern', type=str, default="list_{}_conv", help="file pattern (filled with train/test/valid + other patterns if need be) for ttv files - default 'list_{}_conv'.format(XXX)")
-    argparser.add_argument('--test_only', action='store_true', help="analysis of unlabeled corpuses (takes precedence over --ttv_split)")
+    args = argparser()
+    print(args)
 
-    args = argparser.parse_args()
-    if args.csv_loc is None and args.json_loc is None:
-        raise ValueError("Data location must be given either through the --csv_loc or the --json_loc argument.")
-    elif args.json_loc is not None and not os.path.isdir(args.json_loc):
-        raise ValueError("{args.json_loc} is not a folder.")
-    # not checking csv file, could have other extension but correct format
-
-    if args.select_data is None:
-        if args.output_format == 'sep_txt':
-            args.select_data = ["spa_all", "utterance", "time_stamp", "speaker", "sentence"]
-        if args.output_format == 'one_tsv':
-            args.select_data = ['file_id', 'spa_1', 'spa_2', 'spa_2a', 'speaker', 'sentence']
     spa_subtags = [x for x in args.select_data if re.search("spa_", x) is not None]
+    translation_rename = ('translation' in args.select_data) and not ('sentence' in args.select_data)
+    drop_subset = [col for col in ["sentence", "action"] if col in args.select_data]
+    
     if args.output_format == 'one_tsv':
-        if not args.keep_empty_spa:
-            # split for different files
-            args.select_data = [[x for x in args.select_data if re.search("spa_", x) is None or x == tag] for tag in spa_subtags]
-        else:
-            args.select_data = [args.select_data]
-            spa_subtags = ['spa_all']
+        # split for different files
+        args.select_data = [[x for x in args.select_data if re.search("spa_", x) is None or x == tag] for tag in spa_subtags]
+    
     if args.csv_loc is not None:
-        data = pd.read_csv(args.csv_loc)
-    else: # args.json_loc is None
+        data = pd.read_csv(args.csv_loc, keep_default_na=False) # can't have the 'NA' tag replaced by None! (can use "na_values = ''")
+    else: # args.json_loc is not None
         data = read_lines_from_folder(args.json_loc)
+    # Check translation
+    if translation_rename:
+        print("Renaming 'translation' column as 'sentence'")
+        data["sentence"] = data["translation"]
+    
+    # Step 1: check labels
+    for spa_tag in spa_subtags:
+        if spa_tag != ['spa_all']:
+            labels = dataset_labels(spa_tag.upper())
+            tag_counts = data[spa_tag].value_counts().to_dict()
+            print(f'Tag {spa_tag} proportions:')
+            print({k: np.round(v/data.shape[0],5) for k,v in tag_counts.items()})
+            tag_incorr = [tag for tag in tag_counts.keys() if (tag not in labels.keys())]
+            print('\tRemoving incorrect tags: ', ' '.join(tag_incorr))
+            tag_toofew = [tag for tag, nbocc in tag_counts.items() if (nbocc < args.thres_replace)]
+            print(f'\tRemoving tags with < {args.thres_replace} occurrences: ', ' '.join(tag_toofew))
+            tag_kept = [tag for tag, nbocc in tag_counts.items() if (nbocc >= args.thres_replace) and (tag in labels.keys())]
+            data[spa_tag] = data[spa_tag].fillna('NOL').apply(lambda x: replace_tag(x, tag_incorr, tag_toofew))
 
-    if not args.keep_empty_text:
-        data.dropna(subset=["sentence"], inplace=True)
-        data.drop(data[data["sentence"] =='.'].index, inplace=True)
-    if not (args.keep_empty_spa or args.test_only):
-        data.dropna(subset=[x for x in data.columns if 'spa_' in x], how='any')
-    if args.test_only:
-        args.ttv_split = [0., 1., 0.]
+    # Step 2: Update sentence/actions
+    data[data["sentence"] =='.'] = ''
+    # Step 3: If no sentence and no action and no translation: data is removed
+    data = data[(pd.concat([data[col] != '' for col in drop_subset], axis=1)).any(axis=1)]
+
+    # Step 4: Adapt data based on args (split_documents, duplicate_documents) - TODO
+    
+    # Step 5: Group into train/test/validation (if need be)
     ttv_rep = {f:np.random.choice([0,1,2], p=args.ttv_split) for f in data['file_id'].unique()}
     data["ttv"] = data["file_id"].apply(lambda x: ttv_rep[x])
-
-    print(args)
     
     if args.replace_names:
         for col in ['sentence', 'lemmas']:
             # replace words with capital letter aka proper nouns
             data[col] = data[col].apply(lambda x: x if not isinstance(x, str) else ' '.join([w if (w.capitalize() != w) else replace_pnoun(w) for w in x.split()]))
-
+    if (args.match_age is not None) and ("age_months" in data.columns):
+        match_age = args.match_age if isinstance(args.match_age, list) else [args.match_age]
+        data['age_months'] = data.age_months.apply(lambda age: min(match_age, key=lambda x:abs(x-age)))
 
     if args.output_format == 'sep_txt':
         ttv_writer = { 
@@ -139,15 +266,17 @@ if __name__ == '__main__':
                 1: open(os.path.join('ttv', args.ttv_filepattern.format("test")), 'w'),
                 2: open(os.path.join('ttv', args.ttv_filepattern.format("valid")), 'w')
             }
-        sep_loop(data, args.select_data, ttv_writer, remove_empty_tags=(not args.keep_empty_spa))
+        sep_loop(data, args.select_data, ttv_writer, remove_empty_tags=(not args.keep_untagged))
         for _,v in ttv_writer.items():
             v.close()
     
     elif args.output_format == 'one_tsv':
         ttv_writer = { 
-            0: [os.path.join('ttv', args.ttv_filepattern.format("train_"+x)) for x in spa_subtags],
-            1: [os.path.join('ttv', args.ttv_filepattern.format("test_"+x)) for x in spa_subtags],
-            2: [os.path.join('ttv', args.ttv_filepattern.format("valid_"+x)) for x in spa_subtags]
+            i: [os.path.join('ttv', args.ttv_filepattern.format(fp+"_"+x)) for x in spa_subtags]
+            for i, (fp, ratio) in enumerate(zip(['train', 'test', 'valid'], args.ttv_split)) if ratio > 0.
         }
         # also duplicating line_function
-        one_loop(data, args.select_data, ttv_writer, remove_empty_tags=(not args.keep_empty_spa))
+        one_loop(data, args.select_data, ttv_writer, remove_empty_tags=(not args.keep_untagged))
+    
+    # Logging execution:
+    log_dataset(args)
