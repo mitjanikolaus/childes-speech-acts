Fix dataloading and attention masking

dataset.py
@@ -1,18 +1,24 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
 
 class SpeechActsDataset(Dataset):
 
-    def __init__(self, dataframe, max_len):
+    def __init__(self, dataframe):
+        self.sequence_lengths = [len(sample) for sample in dataframe["features"]]
+        dataframe["features"] = pad_sequence([torch.LongTensor(f) for f in dataframe["features"]], batch_first=True)
         self.len = len(dataframe)
         self.data = dataframe
-        self.max_len = max_len
+        self.max_len = len(dataframe["features"][0])
 
     def __getitem__(self, index):
-        features = self.data[index][0]
-        label = self.data[index][1]
+        features = self.data["features"][index]
+        label = self.data["labels"][index]
 
-        return torch.tensor(features), torch.tensor(label)
+        sequence_length = self.sequence_lengths[index]
+        attention_mask = sequence_length * [1] + (self.max_len - sequence_length) * [0]
+
+        return torch.tensor(features), torch.tensor(label), torch.tensor(attention_mask)
 
     def __len__(self):
         return self.len

models.py
@@ -51,12 +51,12 @@ class LSTMClassifier(nn.Module):
 
 
 class SpeechActDistilBERT(torch.nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, dropout):
         super(SpeechActDistilBERT, self).__init__()
         self.bert = DistilBertModel.from_pretrained("distilbert-base-uncased")
         # TODO optimize size
         self.pre_classifier = torch.nn.Linear(768, 768)
-        self.dropout = torch.nn.Dropout(0.3)
+        self.dropout = torch.nn.Dropout(dropout)
         self.classifier = torch.nn.Linear(768, num_classes)
 
     def forward(self, input_ids, attention_mask):

transformer_train.py
@@ -10,44 +10,34 @@ from torch import cuda
 from dataset import SpeechActsDataset
 from models import SpeechActDistilBERT
 
-device = 'cuda' if cuda.is_available() else 'cpu'
+device = "cuda" if cuda.is_available() else "cpu"
+
 
 def calcuate_accu(big_idx, targets):
-    n_correct = (big_idx==targets).sum().item()
+    n_correct = (big_idx == targets).sum().item()
     return n_correct
 
 
 def train(args):
-    MAX_LEN = 512
-
     vocab = pickle.load(open(args.data + "vocab.p", "rb"))
     label_vocab = pickle.load(open(args.data + "vocab_labels.p", "rb"))
 
     # TODO use BERT tokenizer?
-    train_dataset = pd.read_hdf(args.data +
-                                 "speech_acts_data.h5", 'train')
+    train_dataset = pd.read_hdf(args.data + "speech_acts_data.h5", "train")
 
-    test_dataset = pd.read_hdf(args.data +
-                                 "speech_acts_data.h5", 'test')
+    test_dataset = pd.read_hdf(args.data + "speech_acts_data.h5", "test")
 
+    training_set = SpeechActsDataset(train_dataset)
+    testing_set = SpeechActsDataset(test_dataset)
 
-    training_set = SpeechActsDataset(train_dataset, MAX_LEN)
-    testing_set = SpeechActsDataset(test_dataset, MAX_LEN)
+    train_params = {"batch_size": args.batch_size, "shuffle": True, "num_workers": 0}
 
-    train_params = {'batch_size': args.batch_size,
-                    'shuffle': True,
-                    'num_workers': 0
-                    }
-
-    test_params = {'batch_size': args.batch_size,
-                   'shuffle': True,
-                   'num_workers': 0
-                   }
+    test_params = {"batch_size": args.batch_size, "shuffle": True, "num_workers": 0}
 
     training_loader = DataLoader(training_set, **train_params)
     testing_loader = DataLoader(testing_set, **test_params)
 
-    model = SpeechActDistilBERT(num_classes = len(label_vocab))
+    model = SpeechActDistilBERT(num_classes=len(label_vocab), dropout=args.dropout)
     model.to(device)
 
     loss_function = torch.nn.CrossEntropyLoss()
@@ -59,19 +49,22 @@ def train(args):
         nb_tr_steps = 0
         nb_tr_examples = 0
         model.train()
-        for _, data in enumerate(training_loader, 0):
-            ids = data['ids'].to(device, dtype=torch.long)
-            mask = data['mask'].to(device, dtype=torch.long)
-            targets = data['targets'].to(device, dtype=torch.long)
+        for _, (features, labels, attention_masks) in enumerate(training_loader, 0):
+            features = features.to(device)
+            labels = labels.to(device)
+            attention_masks = attention_masks.to(device)
 
-            outputs = model(ids, mask)
-            loss = loss_function(outputs, targets)
+            # TODO pack sequences?
+            outputs = model(features, attention_masks)
+
+            # TODO take into account different sequence lengths?
+            loss = loss_function(outputs, labels)
             tr_loss += loss.item()
             big_val, big_idx = torch.max(outputs.data, dim=1)
-            n_correct += calcuate_accu(big_idx, targets)
+            n_correct += calcuate_accu(big_idx, labels)
 
             nb_tr_steps += 1
-            nb_tr_examples += targets.size(0)
+            nb_tr_examples += labels.size(0)
 
             if _ % 5000 == 0:
                 loss_step = tr_loss / nb_tr_steps
@@ -84,7 +77,9 @@ def train(args):
             # # When using GPU
             optimizer.step()
 
-        print(f'The Total Accuracy for Epoch {epoch}: {(n_correct * 100) / nb_tr_examples}')
+        print(
+            f"The Total Accuracy for Epoch {epoch}: {(n_correct * 100) / nb_tr_examples}"
+        )
         epoch_loss = tr_loss / nb_tr_steps
         epoch_accu = (n_correct * 100) / nb_tr_examples
         print(f"Training Loss Epoch: {epoch_loss}")
@@ -95,11 +90,10 @@ def train(args):
     for epoch in range(args.epochs):
         train_epoch(epoch)
 
-
         # Saving the files for re-use
         # TODO: only if best model so far
         torch.save(model, args.save)
-        print('Model checkpoint saved')
+        print("Model checkpoint saved")
 
     def valid(model, testing_loader):
         model.eval()
@@ -111,9 +105,9 @@ def train(args):
         nb_tr_examples = 0
         with torch.no_grad():
             for _, data in enumerate(testing_loader, 0):
-                ids = data['ids'].to(device, dtype=torch.long)
-                mask = data['mask'].to(device, dtype=torch.long)
-                targets = data['targets'].to(device, dtype=torch.long)
+                ids = data["ids"].to(device, dtype=torch.long)
+                mask = data["mask"].to(device, dtype=torch.long)
+                targets = data["targets"].to(device, dtype=torch.long)
                 outputs = model(ids, mask).squeeze()
                 loss = loss_function(outputs, targets)
                 tr_loss += loss.item()
@@ -135,12 +129,16 @@ def train(args):
 
         return epoch_accu
 
-    print('This is the validation section to print the accuracy and see how it performs')
     print(
-        'Here we are leveraging on the dataloader crearted for the validation dataset, the approcah is using more of pytorch')
+        "This is the validation section to print the accuracy and see how it performs"
+    )
+    print(
+        "Here we are leveraging on the dataloader crearted for the validation dataset, the approcah is using more of pytorch"
+    )
 
     acc = valid(model, testing_loader)
     print("Accuracy on test data = %0.2f%%" % acc)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -150,23 +148,15 @@ if __name__ == "__main__":
         default="./data/",
         help="location of the data corpus and vocabs",
     )
-    parser.add_argument(
-        "--emsize", type=int, default=300, help="size of word embeddings"
-    )
-    parser.add_argument(
-        "--nhid", type=int, default=200, help="number of hidden units per layer"
-    )
-    parser.add_argument("--nlayers", type=int, default=2, help="number of layers")
-    parser.add_argument("--lr", type=float, default=.001, help="initial learning rate")
-    parser.add_argument("--clip", type=float, default=0.25, help="gradient clipping")
-    parser.add_argument("--epochs", type=int, default=50, help="upper epoch limit")
+    parser.add_argument("--lr", type=float, default=1e-05, help="initial learning rate")
+    parser.add_argument("--epochs", type=int, default=20, help="upper epoch limit")
     parser.add_argument(
         "--batch-size", type=int, default=50, metavar="N", help="batch size"
     )
     parser.add_argument(
         "--dropout",
         type=float,
-        default=0.2,
+        default=0.3,
         help="dropout applied to layers (0 = no dropout)",
     )
     parser.add_argument("--seed", type=int, default=1111, help="random seed")