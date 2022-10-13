cd68b22cbb85f5b16e122914f7bc704cb95ac56c
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-10-13 14:51:31 +0200

    Update frequency graph

--- exp_plot_frequencies.py
+++ exp_plot_frequencies.py
@@ -7,15 +7,16 @@ from utils import SPEECH_ACT, PATH_NEW_ENGLAND_UTTERANCES
 
 if __name__ == "__main__":
     print("Loading data...")
-    # Calculate overall adult speech act frequencies
+    # Calculate overall speech act frequencies
     data = pd.read_pickle(PATH_NEW_ENGLAND_UTTERANCES)
 
     frequencies = calculate_frequencies(data[SPEECH_ACT])
     frequencies = dict(frequencies.most_common())
-
+    plt.figure(figsize=(15, 10))
     plt.bar(list(frequencies.keys()), list(frequencies.values()))
     plt.xlabel("speech act")
     plt.ylabel("frequency")
-    plt.xticks([])
-
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig("results/frequencies_speech_acts_new_england.png", dpi=300)
     plt.show()
